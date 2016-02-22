b46ea91250f718199f1769a19043e3871414f7f5
kevinboogaard <kevin.boogaard3@gmail.com>
2016-02-22 11:39:54 +0100

    Score + switch
    +score
    +switchteam

---
+++ BridgeTrolls_Resurrected/Assets/Scripts/Player/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+    public int score;
+	// Use this for initialization
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == ("Finish"))
+        {
+            score += 1;
+        }
+    }
+}
---
+++ BridgeTrolls_Resurrected/Assets/Scripts/Player/SwitchTeam.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchTeam : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == ("Enemy"))
+        {
+            tag = "Enemy";
+            Destroy(GetComponent<SwitchTeam>());
+            Destroy(GetComponent<Score>());
+            transform.position = new Vector2(0, 0);
+        }
+    }
+}
