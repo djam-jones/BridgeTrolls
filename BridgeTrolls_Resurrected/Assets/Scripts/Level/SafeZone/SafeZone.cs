73339fd0695bb44b36e7bbe8310d2b6be916cfc6
Collix <15986@ma-web.nl>
2016-02-25 10:03:03 +0100

    SafeZone trigger WIP

--- BridgeTrolls_Resurrected/Assets/Scripts/Level/SafeZone/SafeZone.cs
+++ BridgeTrolls_Resurrected/Assets/Scripts/Level/SafeZone/SafeZone.cs
@@ -3,15 +3,7 @@ using System.Collections;
 
 public class SafeZone : MonoBehaviour
 {
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter()
     {
 
     }
