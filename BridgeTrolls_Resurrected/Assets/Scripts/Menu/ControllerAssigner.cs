4a07068add6e42f6515e387748eeab1de163e0c1
Djamali Shawn Jones <15546@ma-web.nl>
2016-04-05 14:33:52 +0200

    New Controller Menu Thing

---
+++ BridgeTrolls_Resurrected/Assets/Scripts/Menu/ControllerAssigner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerAssigner : MonoBehaviour 
+{
+	[SerializeField, HideInInspector] private const string _actionKey = "Submit";
+	[SerializeField, HideInInspector] private const string _cancelKey = "Cancel";
+
+	[SerializeField] private string[] controllerButtonNameArray = new string[8];
+
+	void Update()
+	{
+		for(int i = 0; i < controllerButtonNameArray.Length; i++)
+		{
+			if(Input.GetButtonDown(controllerButtonNameArray[i]))
+			{
+				print(i);
+			}
+		}
+	}
+
+}
